a5d14330abde6ba445f36ac58cfabb4bd325ab04
Héctor <hector.d.rojas.s@gmail.com>
2020-07-18 21:37:14 -0400

    Se añaden entidades

---
+++ src/Entity/Commune.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommuneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommuneRepository::class)
+ */
+class Commune
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Province::class, inversedBy="communes")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $province;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProvince(): ?Province
+    {
+        return $this->province;
+    }
+
+    public function setProvince(?Province $province): self
+    {
+        $this->province = $province;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Region.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RegionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RegionRepository::class)
+ */
+class Region
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Province::class, mappedBy="region", orphanRemoval=true)
+     */
+    private $provinces;
+
+    public function __construct()
+    {
+        $this->provinces = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Province[]
+     */
+    public function getProvinces(): Collection
+    {
+        return $this->provinces;
+    }
+
+    public function addProvince(Province $province): self
+    {
+        if (!$this->provinces->contains($province)) {
+            $this->provinces[] = $province;
+            $province->setRegion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProvince(Province $province): self
+    {
+        if ($this->provinces->contains($province)) {
+            $this->provinces->removeElement($province);
+            // set the owning side to null (unless already changed)
+            if ($province->getRegion() === $this) {
+                $province->setRegion(null);
+            }
+        }
+
+        return $this;
+    }
+}
